8e94501cca53f16ad3d82f61eadb32597ab23b34
nghiem kieu <nghiemkieu95@gmail.com>
2022-02-27 23:08:10 +0700

    udpate

--- web/themes/custom/m2group/js/about.js
+++ web/themes/custom/m2group/js/about.js
@@ -1,19 +1,35 @@
 (function ($, Drupal, drupalSettings) {
     $(document).ready(function () {
         var slide_1 = new Swiper(".about_slide_1", {
-            slidesPerView: 3,
+            slidesPerView: 1.5,
             spaceBetween: 10,
             navigation: {
                 nextEl: ".swiper-button-next",
                 prevEl: ".swiper-button-prev",
             },
+            breakpoints: {
+                640: {
+                    slidesPerView: 2,
+                },
+                1200: {
+                    slidesPerView: 3,
+                },
+            },
         });
         var slide_2 = new Swiper(".about_slide_2", {
-            slidesPerView: 3,
+            slidesPerView: 1.5,
             spaceBetween: 10,
             navigation: {
                 nextEl: ".swiper-button-next",
                 prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                640: {
+                    slidesPerView: 2,
+                },
+                1200: {
+                    slidesPerView: 3,
+                },
             },
         });
     });
